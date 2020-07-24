b2230df5024a84f093b52b1d2b8e243ccd88f783
Rui Hu <boreas955@gmail.com>
2020-07-24 15:44:51 +0800

    添加NoIdea

---
+++ src/com/company/noIdea.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.ArrayList;
+
+public class noIdea {
+
+    ArrayList<String> iUruhAList = new ArrayList<String>();
+
+    public void iUruhNoIdea(){
+        iUruhAList.add("I");
+        iUruhAList.add("AM");
+        iUruhAList.add("UR");
+        iUruhAList.add("FATHER");
+        System.out.println(iUruhAList);
+
+    }
+}
